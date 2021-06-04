9a6bf894a2f8f3db92cfa5b1eac1ff89895a2ee6
Habib826 <habib826826@.com>
2021-06-04 09:39:52 +0600

    adding

---
+++ assets/backgroundImages/index.js
@@ -0,0 +1,6 @@
+const haze = require('./img/haze.jpg');
+const rainy = require('./img/rainy.jpg');
+const winter = require('./img/winter.jpg');
+const sunny = require('./img/sunny.jpg');
+
+export { haze, rainy, winter, sunny };
